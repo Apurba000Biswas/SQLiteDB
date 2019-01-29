9e2702815cceffa0f6a7886fd047d74686fca012
Apurba <apurba10024@gmail.com>
2019-01-29 19:48:16 +0600

    1.2 Added sql file importing and quering data on SQLite db

--- app/src/main/java/com/example/sqlitedb/MainActivity.java
+++ app/src/main/java/com/example/sqlitedb/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.sqlitedb;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +24,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //importDB();
     }
 
+    private void importDB(){
+        // read example.sql into database named "babynames"
+        SQLiteDatabase db = this.openOrCreateDatabase("babynames.db" , this.MODE_PRIVATE, null);
+        Scanner scan = new Scanner(getResources()
+                .openRawResource(R.raw.babynames));
+        String query = "";
+        while (scan.hasNextLine()) { // build and execute queries
+            query += scan.nextLine() + "\n";
+            if (query.trim().endsWith(";")) {
+                db.execSQL(query);
+                query = "";
+            }
+        }
+    }
 
-    public void showGraph(View view) {
+    public void showGraphClicked(View view) {
+        String inputName = ((EditText)findViewById(R.id.et_input_name)).getText().toString();
+        Switch sw = findViewById(R.id.sw_gender);
+        String gender = (sw.isChecked())? "M": "F";
+        SQLiteDatabase db = this.openOrCreateDatabase("babynames.db" , this.MODE_PRIVATE, null);
+
+        String sqlQuery = "SELECT year, rank FROM ranks WHERE name = "
+                + "'" + inputName +"'" + " AND " + "sex = " + "'" + gender + "'";
+
+        Cursor cursor = db.rawQuery(sqlQuery, null);
+        if (cursor.moveToFirst()){
+            do {
+                int year = cursor.getInt(cursor.getColumnIndex("year"));
+                int rank = cursor.getInt(cursor.getColumnIndex("rank"));
+
+                drawGraph(year, rank);
+                //Log.v("MainActivity", "Founded Name Year = " + year + " And Rank = " + rank);
+
+            } while (cursor.moveToNext());
+            cursor.close();
+        }else{
+            Log.v("MainActivity", "Cursor Empty----------");
+        }
+    }
+
+    private void drawGraph(int year, int rank){
         
     }
 }
